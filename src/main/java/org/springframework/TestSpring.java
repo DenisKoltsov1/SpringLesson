61e43fd80f4ec74f472b68ac6eb4b6c3875133c4
DenisKoltsov1 <pivan4207@gmail.com>
2025-04-07 17:12:38 +0300

    Bean scope (Область видимости бинов).
    приминение области видимости для бинов scope и protorype
    Scope, создает один объект(ссылка указывает на один объект), сколько бы не было вызовов  бинов
    prototype создает много объектов(бинов)

--- src/main/java/org/springframework/TestSpring.java
+++ src/main/java/org/springframework/TestSpring.java
@@ -8,10 +8,20 @@ public class TestSpring {
 
         // Music music= context.getBean("musicBean",Music.class)
         // MusicPlayer musicPlayer = new MusicPlayer(music)
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison =firstMusicPlayer == secondMusicPlayer;
+     //   musicPlayer.playMusic();
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+        //System.out.println(musicPlayer.getName())
+        //System.out.println(musicPlayer.getVolume());
         System.out.println();
        context.close();
     }
